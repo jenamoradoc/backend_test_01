e387e5603bbe1748887d2ba1fcdeed23c2f7fdd8
jenamoradoc <davienamorado09@gmail.com>
2018-04-04 15:52:09 -0300

    Development of First modules

---
+++ app.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
---
+++ db.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const initOptions = {
+  // global event notification
+  error: (error, e) => {
+      if (e.cn) {
+        // A connection-related error;
+        //connections are reported back with the password hashed
+        //for sale errors logging, without expossing passwords
+        console.log('CN:' e.cn);
+        console.log('EVENT:', error.message || error);
+      }
+  }
+};
+
+const pgp = require('pg-promise')(initOptions);
+var config = require('config');
+
+var dbConfig = config.get('debConfig'); // from default json
+const db = pgp(dbConfig);
+
+db.connect()
+    .then(obj => {
+        obj.done(); // success, release the connection
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
+
+module.exports = db;
+    
